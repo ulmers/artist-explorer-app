566d0d7437070be302b533f2e2e0b30439579bf9
Stephen Ulmer <stephenulmer@knights.ucf.edu>
2022-10-04 11:19:57 -0400

    STRING -> ENV

--- src/service/ArtistExplorerClient.ts
+++ src/service/ArtistExplorerClient.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "artist-explorer-production.up.railway.app"
+  baseURL: import.meta.env.VITE_API_BASE_URL
 })
 
 export default axiosInstance
